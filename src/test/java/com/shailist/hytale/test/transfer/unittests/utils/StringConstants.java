8c5af6029e544c0cdbea74e5413dead4dcd44d40
shailist <shailist2000@gmail.com>
2025-11-23 06:04:14 +0200

    steam boiler tests

--- src/test/java/com/shailist/hytale/test/transfer/unittests/utils/StringConstants.java
+++ src/test/java/com/shailist/hytale/test/transfer/unittests/utils/StringConstants.java
@@ -6,4 +6,8 @@ public class StringConstants {
 
     public static final long UNIT_MILLIBUCKET = 1L; // Arbitrary number
     public static final long UNIT_BUCKET = 1000L * UNIT_MILLIBUCKET; // Arbitrary number
+    
+    public static final StringVariant LAVA = StringVariant.of("Lava");
+    public static final StringVariant WATER = StringVariant.of("Water");
+    public static final StringVariant STEAM = StringVariant.of("Steam");
 }
